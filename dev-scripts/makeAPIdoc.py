e9b0bea67af5f38be65dc1410f8efc0b1a0433d9
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-03-08 06:45:29 +0100

    Added dynamic python imports

--- dev-scripts/makeAPIdoc.py
+++ dev-scripts/makeAPIdoc.py
@@ -6,21 +6,38 @@
 
 import markdowndoc
 import pydoc
+import imp
+import sys
 
 # Is possible that you want change this object
-import potools
+# import potools
 
 pydoc.text = markdowndoc.MarkdownDoc()
 
+
+def load_clas_from_file(path):
+  class_inst = None
+  expected_class = 'LtspChroot'
+  mod_name,file_ext = os.path.splitext(os.path.split(filepath)[-1])
+  
+  if file_ext.lower() == '.py':
+    py_mod = imp.load_source(mod_name, filepath)
+
+  if hasattr (py_mod, expected_class):
+    class_inst = py_mod.LtspChroot()
+
+  return class_inst
+
 def writeMarkdownAPI():
-    toDocument = [potools]
-    for thing in toDocument:
-        fn = "%s.md" % thing.__name__
-        print("Writing %s..." % fn)
-        f = open(fn, 'w')
-        md = pydoc.render_doc(thing)
-        f.write(md)
-        f.close()
+  toDocument = [LtspChroot]
+  for thing in toDocument:
+    fn = "%s.md" % thing.__name__
+    print("Writing %s..." % fn)
+    f = open(fn, 'w')
+    md = pydoc.render_doc(thing)
+    f.write(md)
+    f.close()
 
 if __name__ == "__main__":
-    writeMarkdownAPI()
+  load_clas_from_file(sys.argv[1]) 
+  writeMarkdownAPI()

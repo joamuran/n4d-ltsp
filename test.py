5f78b4cbad8efe86ca67457dfdb65ffaca67ffc7
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-03-12 17:26:25 +0100

    Fixed test.py

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from xmlrpclib import *
 
 user="aberlanas"
 password="lliurex"
+
 user_info=(user,password)
-#user_info="FRRP6Ww4rYNXGzjJqGedSQKyi7vOjbzfgNPFunl2QvV3VjGjnI"
 server = ServerProxy ("https://localhost:9779") # Conexion con el servidor. Por defecto siempre escucha en el puerto 9779
-print server.get_lliurex_version_on_chroot(user_info,"LtspChroot","pepito el de los patoles","/graveyard/")
+
+print (server.get_lliurex_version_on_chroot(user_info,"LtspChroot","/graveyard/"))
 

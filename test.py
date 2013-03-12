b996e1e3d988b7efcdb02f248f320dd2cfa79813
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-03-12 17:37:22 +0100

    Added more logic

--- test.py
+++ test.py
@@ -10,18 +10,26 @@ user="user"
 password="password"
 
 
-# Get the user
-if (sys.argv[0]): 
-  user=sys.argv[0]
+# Simple mechanism to support given the user and password as a arguments
+# this is a not "production mechanism".
 
-# Get the password
-if (sys.argv[1]):
-  password = sys.argv[1]
+try:
+
+  # Get the user
+  if (sys.argv[0]): 
+    user=sys.argv[0]
+
+  # Get the password
+  if (sys.argv[1]):
+    password = sys.argv[1]
+
+except Exception as e:
+  print("Exception is ocurred, default user password is set")
+
 
 user_info=(user,password)
-
-print (user_info)
+print ("[DEBUG] :: " + (str)user_info)
 server = ServerProxy ("https://localhost:9779") # Conexion con el servidor. Por defecto siempre escucha en el puerto 9779
 
-print (server.get_lliurex_version_on_chroot(user_info,"LtspChroot","/graveyard/"))
+#print (server.get_lliurex_version_on_chroot(user_info,"LtspChroot","/graveyard/"))
 

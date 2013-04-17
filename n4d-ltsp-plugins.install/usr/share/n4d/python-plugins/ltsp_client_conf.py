be2a3cc141c26007bbd9f864d52f09e9167cef34
José A. Múrcia <joamuran@gmail.com>
2013-04-17 14:24:39 +0200

    WIP on  read/write lts.conf

--- n4d-ltsp-plugins.install/usr/share/n4d/python-plugins/ltsp_client_conf.py
+++ n4d-ltsp-plugins.install/usr/share/n4d/python-plugins/ltsp_client_conf.py
@@ -3,6 +3,9 @@
 # Libraries
 import sys
 import re
+import json
+import urllib
+
 #import subprocess
 
 class Client:
@@ -16,22 +19,23 @@ class Client:
     self.autologin="unchecked"
     self.username=""
     self.userpass=""
-  def set_type(self, clienttype):
-    self.fat=clienttype
-  def set_name(self, name):
-    self.name=name
-  def set_desc(self, desc):
-    self.desc=desc
-  def set_session(self, session):
-    self.session=session
-  def set_monitor(self, monitor):
-    self.monitor=monitor
-  def set_autologin(self, autologin):
-    self.autologin=autologin
-  def set_username(self, username):
-    self.username=username
-  def set_usernpass(self, userpass):
-    self.userpass=userpass
+    
+  def set_type(self, param):
+    self.fat=param  
+  def set_name(self, param):
+    self.name=param
+  def set_desc(self, param):
+    self.desc=param
+  def set_session(self, param):
+    self.session=param
+  def set_monitor(self, param):
+    self.monitor=param
+  def set_autologin(self, param):
+    self.autologin=param
+  def set_username(self, param):
+    self.username=param
+  def set_userpass(self, param):
+    self.userpass=param
   def get_client(self):
     client='{'
     client=client + '"mac":"'+self.mac+'"'
@@ -43,6 +47,9 @@ class Client:
     client=client + ',"autologin":"'+self.autologin+'"'
     client=client + ',"username":"'+self.username+'"'
     client=client + ',"userpass":"'+self.userpass+'"}'
+    print "*"
+    print client
+    print "!"
     return client
 
 
@@ -73,6 +80,7 @@ class LtspClientConfig:
       line=line.strip()
       #First, let's see if is not a comment
       if len(line)>0 and line[0]!="#":
+        #print "line: "+line
         # Let's see if it's a section definition...
         if line[0]=="[":
           # is default section?
@@ -96,26 +104,30 @@ class LtspClientConfig:
         else:
           if self.current_section!="" and self.current_section!="default":
             if line[:8]=="LTSP_FAT":
-              self.current_client.set_type=line[15:]
-              #print "FatClient: "+line[15:]
+              self.current_client.set_type(urllib.quote(line[15:]))
+              #print "FatClient: "+line[14:]
             if line[:8]=="HOSTNAME":
-              self.current_client.set_name=line[10:]
-              #print "host: "+line[10:]
+              self.current_client.set_name(urllib.quote(line[9:]))
+              #print "host: "+line[9:]
             if line[:8]=="LDM_SESS":
-              self.current_client.set_session=line[12:]
+              self.current_client.set_session(urllib.quote(line[12:]))
               #print "session: "+line[12:]
             if line[:8]=="LDM_AUTO":
-              self.current_client.set_autologin=line[14:]
+              self.current_client.set_autologin(urllib.quote(line[14:]))
               #print "Autologin: "+line[14:]
             if line[:8]=="LDM_USER":
-              self.current_client.set_username=line[13:]
+              self.current_client.set_username(urllib.quote(line[13:]))
               #print "username: "+line[13:]
             if line[:8]=="LDM_PASS":
-              self.current_client.set_userpass=line[13:]
+              self.current_client.set_userpass(urllib.quote(line[13:]))
               #print "password: "+line[13:]
             
             #print line[:7]
-           #print self.current_section+"> "+line
+            #print self.current_section+"> "+line
+      else: ##if len(line)>0 and line[0]!="#":
+        if len(line)>0 and line[:8]=="#LLX-Des":
+          self.current_client.set_desc(urllib.quote(line[12:]))
+
     # END for line in lines:
     if self.current_client is not None:
       # Write the last client
@@ -136,11 +148,36 @@ class LtspClientConfig:
     
   
   def set_ltsp_conf(self, config):
-    print "*************"
-    print config
-    print "*************"
-    pass
-  
-#client=LtspClientConfig()
-#conf=client.get_ltsp_conf()
+    self.new_conf_file="/var/lib/tftpboot/ltsp/lts.conf"
+    self.template_file="/var/lib/lliurex-ltsp/templates/lts.conf"
+    readlines=open(self.template_file, 'r').readlines();
+    writefile=open(self.new_conf_file, 'w');
+    for line in readlines:
+      writefile.write(line)
+    
+    clientlist=json.loads(config)
+    for client in clientlist["clients"]:
+      writefile.write("\n\n["+client["mac"]+"]")
+      if client["desc"]!="":
+        writefile.write("\n#LLX-Desc="+client["desc"])
+      if client["name"]!="":
+        writefile.write("\nHOSTNAME="+client["name"])
+      if client["fat"]!="":
+        writefile.write("\nLTSP_FATCLIENT="+client["fat"])
+      if client["session"]=="gnome":
+        writefile.write('\nLDM_SESSION="gnome-fallback"')
+      elif client["session"]=="lxde":
+        writefile.write('\nLDM_SESSION=/usr/bin/startlxde')
+      # ignoring monitor
+      if client["autologin"]=="checked":
+        writefile.write('\nLDM_AUTOLOGIN=True')
+      if client["username"]!="":
+        writefile.write("\nLDM_USERNAME="+client["username"])
+      if client["userpass"]!="":
+        writefile.write("\nLDM_PASSWORD="+client["userpass"])
+
+
+client=LtspClientConfig()
+print client.get_ltsp_conf()
+#conf=client.set_ltsp_conf('{"clients":[{"mac":"A1:B1:C2:D4:E5:F6","fat":"true","name":"n1","desc":"desc1","session":"gnome","monitor":"auto","autologin":"unchecked","username":"","userpass":""},{"mac":"A1:B1:C2:D4:E5:F6","fat":"true","name":"","desc":"","session":"lxde","monitor":"auto","autologin":"checked","username":"user","userpass":"123"}]}')
 #print conf

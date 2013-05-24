28fa0b4b42796bbb66dff802b6236a717117ee4b
José A. Múrcia <joamuran@gmail.com>
2013-05-24 13:44:31 +0200

    Fixed

--- n4d-ltsp-plugins.install/usr/share/n4d/python-plugins/N4dRemoteLog.py
+++ n4d-ltsp-plugins.install/usr/share/n4d/python-plugins/N4dRemoteLog.py
@@ -2,88 +2,17 @@
 
 # Libraries
 import sys
-import urllib
-import logging
-import logging.handlers
-import SocketServer
-import struct
 import time
 import os
-import pickle
-
+import lliurex.net
 #import subprocess
 
-class LogRecordStreamHandler(SocketServer.StreamRequestHandler):
-    """Handler for a streaming logging request.
-
-    This basically logs the record using whatever logging policy is
-    configured locally.
-    """
-
-    def handle(self):
-        """
-        Handle multiple requests - each expected to be a 4-byte length,
-        followed by the LogRecord in pickle format. Logs the record
-        according to whatever policy is configured locally.
-        """
-        while True:
-            chunk = self.connection.recv(4)
-            if len(chunk) < 4:
-                break
-            slen = struct.unpack('>L', chunk)[0]
-            chunk = self.connection.recv(slen)
-            while len(chunk) < slen:
-                chunk = chunk + self.connection.recv(slen - len(chunk))
-            obj = self.unPickle(chunk)
-            record = logging.makeLogRecord(obj)
-            self.handleLogRecord(record)
-
-    def unPickle(self, data):
-        return pickle.loads(data)
-
-    def handleLogRecord(self, record):
-        # if a name is specified, we use the named logger rather than the one
-        # implied by the record.
-        if self.server.logname is not None:
-            name = self.server.logname
-        else:
-            name = record.name
-        logger = logging.getLogger(name)
-        # N.B. EVERY record gets logged. This is because Logger.handle
-        # is normally called AFTER logger-level filtering. If you want
-        # to do filtering, do it at the client end to save wasting
-        # cycles and network bandwidth!
-        logger.handle(record)
-
-class LogRecordSocketReceiver(SocketServer.ThreadingTCPServer):
-  """
-  Simple TCP socket-based logging receiver suitable for testing.
-  """
-
-  allow_reuse_address = 1
-
-  def __init__(self, host='localhost',
-              port=logging.handlers.DEFAULT_TCP_LOGGING_PORT,
-              handler=LogRecordStreamHandler):
-    SocketServer.ThreadingTCPServer.__init__(self, (host, port), handler)
-    self.abort = 0
-    self.timeout = 1
-    self.logname = None
-
-  def serve_until_stopped(self):
-    import select
-    abort = 0
-    while not abort:
-      rd, wr, ex = select.select([self.socket.fileno()],
-                                [], [],
-                                self.timeout)
-      if rd:
-        self.handle_request()
-      abort = self.abort
 
 class N4dRemoteLog:
   
   fd=None
+  LOG_PATH="/tmp/"
+  LOG_PREFIX="n4dr"
   
   def info(self):
     '''
@@ -92,41 +21,57 @@ class N4dRemoteLog:
     return {'status':True, 'msg':'[N4dRemoteLog] Manage remote logs'}
   
   #def info
-  
-  
-  
+
   def __init__(self):
     pass    
-  
-  def create_logging_server(self, log_server, port=9999 ):
-    '''
-    Create the loggin server on the custom port
-    '''
-    logging.basicConfig(
-        format='%(relativeCreated)5d %(name)-15s %(levelname)-8s %(message)s')
-    tcpserver = LogRecordSocketReceiver()
-    print('[N4DRemoteLog] About to start TCP server for Logging...')
-    tcpserver.serve_until_stopped()
-  
-  
-  def write_log(self, text):
-    '''
-    Write the text to the log and publish it at Loggin System
-    '''
+  # def __init__
     
-    pass
-
   
-  def read_log(self, line):
-    pass
+  def read_n4dr_log(self, log_name, line_start, line_nums):
+    '''
+    Read logging from server passed as name.
+    Starting at the line_start argument to the line_start+line_nums
+    If the end of file is foundit returns all file.
+    
+    If only filename is passed (and None None), the entire file is
+    sended
+    '''
+    log_name_fullpath=N4dRemoteLog.LOG_PATH+N4dRemoteLog.LOG_PREFIX+log_name+".log"
+    
+    if line_start != None and line_nums != None:
+      line_start=int(line_start)
+      line_end=int(line_start)+int(line_nums)
+      if os.path.exists(log_name_fullpath):
+        lines = open(log_name_fullpath, 'r').readlines()[line_start:line_end]
+    else:
+      if os.path.exists(log_name_fullpath):
+        lines = open(log_name_fullpath, 'r').readlines()
+        
+    print(lines)
+    
+    return {'status':True, 'file':lines}
+  #def read_log_lines_from_server(self, filename, line_start, line_nums)
   
-
+  def write_n4dr_log(self,log_name,log_msg):
+    '''
+    Write the log_msg to the ip_to_write with the logname
+    '''
+    if log_msg != None :
+        log_name_fullpath=N4dRemoteLog.LOG_PATH+N4dRemoteLog.LOG_PREFIX+log_name+".log"
+        print log_name_fullpath
+        try:
+            f=open(log_name_fullpath,'a')
+            f.write(log_msg+"\n")
+            f.close()
+        except Exception as e:
+            return {'status':False, 'msg':str(e)}
+    
+    return {'status':True, 'file':str(log_name)}
+  
 if __name__ == '__main__':
   
   n4dLog = N4dRemoteLog()
-  n4dLog.create_logging_server("test",9999)
-  
+
 
   
-
 
